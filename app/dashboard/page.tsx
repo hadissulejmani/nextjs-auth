2b33f70eb13bdfaf6beb696d1746323c121c95dc
Hadis Sulejmani <hadissulejmani@outlook.com>
2024-06-04 15:20:12 +0200

    layout fix

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+    return (
+        <div className="flex justify-center py-24">
+            <div className="w-full max-w-md">
+                <h1 className="text-3xl font-bold">Dashboard</h1>
+            </div>
+        </div>
+    );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,11 @@ import {
   UserButton
 } from '@clerk/nextjs'
 import './globals.css'
+import './globals.css';
+import Script from 'next/script';
+import { Metadata } from 'next';
+import { GeistSans } from 'geist/font/sans';
+import { GeistMono } from 'geist/font/mono';
 
 export const metadata = {
   title: 'Next.js Authentication Template',
@@ -18,18 +23,27 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <ClerkProvider>
-      <html lang="en">
-        <body>
-          <SignedOut>
-            <SignInButton />
-          </SignedOut>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
-          {children}
-        </body>
-      </html>
-    </ClerkProvider>
-  )
+    <html lang="en" className={`${GeistSans.variable} ${GeistMono.variable}`}>
+      <ClerkProvider
+        appearance={{
+          variables: { colorPrimary: '#000000' },
+          elements: {
+            formButtonPrimary: 'bg-black border border-black border-solid hover:bg-white hover:text-black',
+            socialButtonsBlockButton:
+              'bg-white border-gray-200 hover:bg-transparent hover:border-black text-gray-600 hover:text-black',
+            socialButtonsBlockButtonText: 'font-semibold',
+            formButtonReset:
+              'bg-white border border-solid border-gray-200 hover:bg-transparent hover:border-black text-gray-500 hover:text-black',
+            membersPageInviteButton: 'bg-black border border-black border-solid hover:bg-white hover:text-black',
+            card: 'bg-[#fafafa]',
+          },
+        }}
+      >
+        <body className={`min-h-screen flex flex-col antialiased`}>{children}</body>
+      </ClerkProvider>
+
+      <Script src="https://cdn.jsdelivr.net/npm/prismjs@1/components/prism-core.min.js" />
+      <Script src="https://cdn.jsdelivr.net/npm/prismjs@1/plugins/autoloader/prism-autoloader.min.js" />
+    </html>
+  );
 }
