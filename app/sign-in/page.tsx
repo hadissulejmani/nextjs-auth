646ebd8180de3f2bf8a69f8eb1e7c2887441ff08
Hadis Sulejmani <hadissulejmani@outlook.com>
2024-06-09 13:44:54 +0200

    auth fixes

---
+++ app/sign-in/page.tsx
@@ -0,0 +1,12 @@
+// app/signin/page.tsx
+import React from 'react';
+import { SignIn } from '../components/sign-in';
+
+export default function Page() {
+  return (
+    <div>
+      <SignIn />
+    </div>
+  );
+};
+
