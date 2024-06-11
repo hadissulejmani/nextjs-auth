aaa8b9945c040a87c518d8d7e56bc2b12025a058
Hadis Sulejmani <hadissulejmani@outlook.com>
2024-06-11 16:53:45 +0200

    new signin action

--- app/components/sign-in.tsx
+++ app/components/sign-in.tsx
@@ -37,7 +37,6 @@ export function SignIn() {
                 id="email"
                 type="email"
                 placeholder="m@example.com"
-                required
               />
             </div>
             <div className="grid gap-2">
@@ -47,18 +46,18 @@ export function SignIn() {
                   Forgot your password?
                 </Link>
               </div>
-              <Input id="password" type="password" required />
+              <Input id="password" type="password" />
             </div>
             <Button type="submit" className="w-full">
               Login
             </Button>
-            <Button type="submit" variant="outline" className="w-full">
+            <Button type="submit" name="action" value="google" variant="outline" className="w-full">
               Signin with Google
             </Button>
           </div>
           <div className="mt-4 text-center text-sm">
             Don&apos;t have an account?{" "}
-            <Link href="#" className="underline">
+            <Link href="/sign-up" className="underline">
               Sign up
             </Link>
           </div>
@@ -68,49 +67,3 @@ export function SignIn() {
   )
 }
 
-export function LoginForm() {
-  return (
-    <Card className="mx-auto max-w-sm">
-      <CardHeader>
-        <CardTitle className="text-2xl">Login</CardTitle>
-        <CardDescription>
-          Enter your email below to login to your account
-        </CardDescription>
-      </CardHeader>
-      <CardContent>
-        <div className="grid gap-4">
-          <div className="grid gap-2">
-            <Label htmlFor="email">Email</Label>
-            <Input
-              id="email"
-              type="email"
-              placeholder="m@example.com"
-              required
-            />
-          </div>
-          <div className="grid gap-2">
-            <div className="flex items-center">
-              <Label htmlFor="password">Password</Label>
-              <Link href="#" className="ml-auto inline-block text-sm underline">
-                Forgot your password?
-              </Link>
-            </div>
-            <Input id="password" type="password" required />
-          </div>
-          <Button type="submit" className="w-full">
-            Login
-          </Button>
-          <Button variant="outline" className="w-full">
-            Login with Google
-          </Button>
-        </div>
-        <div className="mt-4 text-center text-sm">
-          Don&apos;t have an account?{" "}
-          <Link href="#" className="underline">
-            Sign up
-          </Link>
-        </div>
-      </CardContent>
-    </Card>
-  )
-}
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,28 @@
-export default function Page() {
+"use client";
+
+import { auth, signOut } from "@/auth";
+import { Button } from "@/components/ui/button";
+import { redirect } from "next/navigation";
+
+
+const Dashboard = async () => {
+    {
+        "use server";
+        const session = await auth();
+        if (!session?.user) redirect("/");
+    }
+    const session = await auth();
+    
+    if (!session?.user) redirect("/");
+
     return (
         <div className="flex justify-center py-24">
             <div className="w-full max-w-md">
                 <h1 className="text-3xl font-bold">Dashboard</h1>
+                <Button onClick={() => signOut()}>Sign out</Button>
             </div>
         </div>
     );
 }
+
+export default Dashboard;
