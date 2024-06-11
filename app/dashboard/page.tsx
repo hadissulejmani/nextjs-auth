85f42d6227068b2df66fd5d72ca362a9ac14c71d
Hadis Sulejmani <hadissulejmani@outlook.com>
2024-06-11 17:50:54 +0200

    users data in the dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,3 @@
-"use client";
 
 import { auth, signOut } from "@/auth";
 import { Button } from "@/components/ui/button";
@@ -19,7 +18,21 @@ const Dashboard = async () => {
         <div className="flex justify-center py-24">
             <div className="w-full max-w-md">
                 <h1 className="text-3xl font-bold">Dashboard</h1>
-                <Button onClick={() => signOut()}>Sign out</Button>
+                <p className="mt-2 text-sm text-gray-600">
+                    Welcome {session?.user?.name}!
+                </p>
+                <p className="mt-4 text-sm text-gray-600">
+                    User email: <code>{session?.user?.email}</code>
+                </p>
+                <p className="mt-4 text-sm text-gray-600">
+                    User id: <code>{session?.user?.id}</code>
+                </p>
+                <Image
+                    src={session?.user?.image}
+                    alt={session?.user?.name}
+                    width={180}
+                    height={37}
+                    className="rounded-full" />
             </div>
         </div>
     );
