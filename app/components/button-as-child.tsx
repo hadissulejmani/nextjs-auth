60ae9314609638e8e6ac86ea51af54d89c776e10
Hadis Sulejmani <hadissulejmani@outlook.com>
2024-06-13 17:51:15 +0200

    Enable passing button classes as props

--- app/components/button-as-child.tsx
+++ app/components/button-as-child.tsx
@@ -3,12 +3,13 @@ import { Button } from "@/components/ui/button"
 
 type ButtonAsChildProps = {
   path?: string;
-  value?: string;
+  value: string;
+  className?: string;
 }
 
-export function ButtonAsChild({ path = "/sign-in", value = "Login" }: ButtonAsChildProps) {
+export function ButtonAsChild({ path = "/sign-in", value = "Login", className }: ButtonAsChildProps) {
   return (
-    <Button asChild className={`bg-blue-500 text-white hover:bg-blue-600 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center w-full`}>
+    <Button asChild className={className}>
       <Link href={path}>{value}</Link>
     </Button>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -10,8 +10,8 @@ export default function Home() {
           <h1 className="text-xl font-bold">Authentication with Google App</h1>
         </div>
         <div className="relative flex flex-col lg:flex-row justify-center gap-5 z-10 items-center w-full lg:w-auto h-full mt-10 lg:mt-0">
-          <ButtonAsChild path="/sign-in" value="Sign In" />
-          <ButtonAsChild path="/sign-up" value="Sign Up" />
+          <ButtonAsChild path="/sign-in" value="Sign In" className="bg-blue-500 text-white hover:bg-blue-600 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center w-full"/>
+          <ButtonAsChild path="/sign-up" value="Sign Up" className="bg-blue-900 text-white hover:bg-blue-600 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center w-full" />
         </div>
       </div>
     </main>
