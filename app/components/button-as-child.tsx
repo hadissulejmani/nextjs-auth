dcc8ddc500f57308be27b2c6c0b288f0b334d322
Hadis Sulejmani <hadissulejmani@outlook.com>
2024-06-13 16:52:42 +0200

    button style customized

--- app/components/button-as-child.tsx
+++ app/components/button-as-child.tsx
@@ -8,7 +8,7 @@ type ButtonAsChildProps = {
 
 export function ButtonAsChild({ path = "/sign-in", value = "Login" }: ButtonAsChildProps) {
   return (
-    <Button asChild>
+    <Button asChild className={`bg-blue-500 text-white hover:bg-blue-600 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center w-full`}>
       <Link href={path}>{value}</Link>
     </Button>
   )
